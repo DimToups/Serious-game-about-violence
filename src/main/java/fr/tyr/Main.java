c36f341cbe2fd51bc062f4b9a4be3bbf168c73bc
Czekaj Tom <red57101@gmail.com>
2023-09-17 13:52:03 +0200

    :sparkles: Add main class

--- src/main/java/fr/tyr/Main.java
+++ src/main/java/fr/tyr/Main.java
@@ -1,7 +1,18 @@
 package fr.tyr;
 
+import fr.tyr.game.GameEngine;
+import fr.tyr.game.GraphicEngine;
+import fr.tyr.game.GameWindow;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean devMode = false;
+        if(args.length > 0)
+            devMode = args[0].equals("--dev");
+        GameEngine gameEngine = new GameEngine(devMode);
+        GraphicEngine graphicEngine = new GraphicEngine(gameEngine);
+        GameWindow gameWindow = new GameWindow(graphicEngine);
+        gameWindow.add(graphicEngine);
+        gameWindow.setVisible(true);
     }
 }
