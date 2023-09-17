4d8de80fbb14e06b6a70f740ed66bda208c5557c
Czekaj Tom <red57101@gmail.com>
2023-09-17 13:32:34 +0200

    :rocket: Add Runner class for multi-threading

---
+++ src/main/java/fr/tyr/tools/Runner.java
@@ -0,0 +1,134 @@
+package fr.tyr.tools;
+
+import com.google.common.collect.EvictingQueue;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+import java.util.Queue;
+import java.util.concurrent.locks.ReentrantLock;
+
+@SuppressWarnings("unused")
+public class Runner extends Thread{
+
+    private final Runnable action;
+    private double aps;
+    private final boolean stability;
+
+    private boolean running = false;
+    private boolean paused = false;
+    private int passedActions = 0;
+    private Queue<Long> apsQueue;
+    private final ReentrantLock queueLock = new ReentrantLock();
+
+    /**
+     * Create a new Runner
+     * @param action The action to run
+     */
+    public Runner(@NotNull final Runnable action){
+        this(action, 60, true);
+    }
+
+    /**
+     * Create a new Runner
+     * @param action The action to run
+     * @param aps The number of actions per second
+     * @param stabilized If the runner should be stable
+     */
+    public Runner(@NotNull final Runnable action, @Range(from = 0, to=10000) final int aps, final boolean stabilized){
+        this.action = action;
+        this.aps = 1D / aps;
+        this.stability = stabilized;
+        this.apsQueue = EvictingQueue.create(aps / 2);
+    }
+
+    @Override
+    public void run() {
+        this.running = true;
+        long nextAction = (long) (System.nanoTime() + this.aps * 1_000_000_000L);
+        long start = System.nanoTime();
+        while (running){
+            if(!paused){
+                if(System.nanoTime() >= nextAction){
+                    if(this.stability)
+                        nextAction = (long) (nextAction + this.aps * 1_000_000_000L);
+                    else
+                        nextAction = (long) (System.nanoTime() + this.aps * 1_000_000_000L);
+                    try{
+                        this.action.run();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    this.passedActions++;
+                    long end = System.nanoTime();
+                    this.queueLock.lock();
+                    try {
+                        this.apsQueue.add(end - start);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }finally {
+                        this.queueLock.unlock();
+                    }
+                    start = System.nanoTime();
+                }
+            }
+            Thread.yield();
+        }
+        this.queueLock.lock();
+        try {
+            this.apsQueue.clear();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            this.queueLock.unlock();
+        }
+    }
+
+    public void setPaused(boolean paused) {
+        this.paused = paused;
+    }
+    public void stopRunner(){
+        this.running = false;
+    }
+    public void setAps(int aps){
+        this.aps = 1D / aps;
+        this.queueLock.lock();
+        try {
+            this.apsQueue = EvictingQueue.create(aps);
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            this.queueLock.unlock();
+        }
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+    public int getPassedActions() {
+        return passedActions;
+    }
+    public int getCurrentAps() {
+        return (int) (1D / (computeAverage() / 1_000_000_000D));
+    }
+    public int getAps() {
+        return (int) (1D / this.aps);
+    }
+
+    private double computeAverage() {
+        long sum = 0;
+        int count = this.apsQueue.size();
+        this.queueLock.lock();
+        try{
+            for (long element : this.apsQueue)
+                sum += element;
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            this.queueLock.unlock();
+        }
+        if (count > 0)
+            return (double) sum / count;
+        else
+            return -1_000_000_000;
+    }
+}
