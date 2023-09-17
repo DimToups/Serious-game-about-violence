8fd5b1bb7f903f41e44de33a42ba930fc96bcf4d
Czekaj Tom <red57101@gmail.com>
2023-09-17 13:32:17 +0200

    :sparkles: Add sound system

---
+++ src/main/java/fr/tyr/sounds/Sounds.java
@@ -0,0 +1,67 @@
+package fr.tyr.sounds;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.sound.sampled.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+@SuppressWarnings("unused")
+public enum Sounds {
+    DING("ding.wav"),
+    BACKGROUND("background.wav");
+
+    private final String path;
+    private Clip clip;
+    private LineListener lineListener;
+
+    Sounds(@NotNull final String path){
+        this.path = path;
+    }
+
+    private void loadSound(){
+        try {
+            URL audioUrl = getClass().getResource("/" + this.path);
+            if(Objects.isNull(audioUrl))
+                throw new NullPointerException("The path " + this.path + " is invalid");
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioUrl);
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void play(){
+        if(Objects.isNull(clip))
+            loadSound();
+        clip.start();
+    }
+
+    public void playBackground(){
+        if(Objects.isNull(clip))
+            loadSound();
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stop(){
+        if(Objects.nonNull(clip))
+            clip.stop();
+    }
+
+    public void setVolume(float volume){
+        if(Objects.isNull(clip))
+            loadSound();
+        if (volume < 0f || volume > 1f)
+            throw new IllegalArgumentException("Volume not valid: " + volume);
+        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        gainControl.setValue(20f * (float) Math.log10(volume));
+    }
+
+    public void restart(){
+        if(Objects.isNull(clip))
+            loadSound();
+        clip.setFramePosition(0);
+    }
+}
