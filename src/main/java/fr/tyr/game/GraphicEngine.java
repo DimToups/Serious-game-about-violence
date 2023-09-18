bef990f00a2b6116002bcbe9bae5b316939ee87b
Czekaj Tom <red57101@gmail.com>
2023-09-18 11:06:33 +0200

    :zap: Improve lock usage

--- src/main/java/fr/tyr/game/GraphicEngine.java
+++ src/main/java/fr/tyr/game/GraphicEngine.java
@@ -23,17 +23,17 @@ public class GraphicEngine extends JPanel {
 
     public GraphicEngine(GameEngine gameEngine){
         this.gameEngine = gameEngine;
-        this.setBackground(Color.BLACK);
+        setBackground(Color.BLACK);
         // Init components
         // Runner setup
-        this.fpsRunner = new Runner(() -> {
-            this.paintImmediately(this.getBounds());
+        fpsRunner = new Runner(() -> {
+            paintImmediately(getBounds());
         }, 60, false);
-        this.tpsRunner = new Runner(this::tick, 60, false);
-        this.fpsRunner.start();
-        this.tpsRunner.start();
+        tpsRunner = new Runner(this::tick, 60, false);
+        fpsRunner.start();
+        tpsRunner.start();
         // Click listener
-        this.addMouseListener(new MouseAdapter() {
+        addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 onClick(e);
@@ -47,46 +47,45 @@ public class GraphicEngine extends JPanel {
     }
 
     private void onClick(MouseEvent event){
-        this.gameEngine.getComponentsLock().lock();
-        try{
-            Vector2D mouseVector = new Vector2D(event.getX(), event.getY());
-            List<GameComponent> localComponents = new ArrayList<>(this.gameEngine.getComponents());
-            Collections.reverse(localComponents);
-            for(GameComponent component : localComponents){
-                Vector2D componentPosition = component.getPosition();
-                Vector2D componentSize = component.getSize();
-                boolean isMouseBetween = mouseVector.isBetween(componentPosition, Vector2D.add(componentPosition, componentSize));
-                if(isMouseBetween){
-                    component.onClick();
-                    return;
-                }
+        List<GameComponent> localComponents = getReversedComponentsList();
+        Collections.reverse(localComponents);
+        Vector2D mouseVector = new Vector2D(event.getX(), event.getY());
+        for(GameComponent component : localComponents){
+            Vector2D componentPosition = component.getPosition();
+            Vector2D componentSize = component.getSize();
+            boolean isMouseBetween = mouseVector.isBetween(componentPosition, Vector2D.add(componentPosition, componentSize));
+            if(isMouseBetween){
+                component.onClick();
+                return;
             }
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            this.gameEngine.getComponentsLock().unlock();
         }
     }
 
     private void tick(){
-        this.gameEngine.getComponentsLock().lock();
-        try{
-            List<GameComponent> localComponents = new ArrayList<>(this.gameEngine.getComponents());
-            Collections.reverse(localComponents);
+        List<GameComponent> localComponents = getReversedComponentsList();
+        Collections.reverse(localComponents);
+        for(GameComponent component : localComponents)
+            component.move(tpsRunner.getAps());
+        Point mouseLocation = getMousePosition();
+        if(Objects.nonNull(mouseLocation)){
+            Vector2D mouseVector = new Vector2D(mouseLocation.x, mouseLocation.y);
+            boolean hoverFound = false;
             for(GameComponent component : localComponents)
-                component.move(this.tpsRunner.getAps());
-            Point mouseLocation = this.getMousePosition();
-            if(Objects.nonNull(mouseLocation)){
-                Vector2D mouseVector = new Vector2D(mouseLocation.x, mouseLocation.y);
-                boolean hoverFound = false;
-                for(GameComponent component : localComponents)
-                    hoverFound = this.triggerHover(mouseVector, component, hoverFound);
-            }
+                hoverFound = triggerHover(mouseVector, component, hoverFound);
+        }
+    }
+
+    private List<GameComponent> getReversedComponentsList(){
+        List<GameComponent> localComponents = new ArrayList<>();
+        gameEngine.getComponentsLock().lock();
+        try{
+            localComponents.addAll(gameEngine.getComponents());
         }catch (Exception e){
             e.printStackTrace();
         }finally {
-            this.gameEngine.getComponentsLock().unlock();
+            gameEngine.getComponentsLock().unlock();
         }
+        return localComponents;
     }
 
     private boolean triggerHover(Vector2D mouseVector, GameComponent component, boolean hoverFound) {
@@ -114,37 +113,37 @@ public class GraphicEngine extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        this.drawComponents(g);
-        Point mouseLocation = this.getMousePosition();
-        if(Objects.nonNull(mouseLocation) && 0 < mouseLocation.x && mouseLocation.x < this.getWidth() && 0 < mouseLocation.y && mouseLocation.y < this.getHeight()){
+        drawComponents(g);
+        Point mouseLocation = getMousePosition();
+        if(Objects.nonNull(mouseLocation)){
             g.setColor(Color.WHITE);
             g.fillOval(mouseLocation.x - 20, mouseLocation.y - 20, 40, 40);
         }
         g.setColor(Color.WHITE);
         g.setFont(font);
-        if(this.gameEngine.isDevMode()){
-            g.drawString("TPS : %d".formatted(this.tpsRunner.getCurrentAps()), 10, 50);
+        if(gameEngine.isDevMode()){
+            g.drawString("TPS : %d".formatted(tpsRunner.getCurrentAps()), 10, 50);
             long end = System.nanoTime();
-            g.drawString("FPS : %d    %d ms".formatted(this.fpsRunner.getCurrentAps(), (end - start) / 1000000L), 10, 25);
+            g.drawString("FPS : %d    %d ms".formatted(fpsRunner.getCurrentAps(), (end - start) / 1000000L), 10, 25);
         }
     }
 
     private void drawComponents(Graphics g){
-        this.gameEngine.getComponentsLock().lock();
+        gameEngine.getComponentsLock().lock();
         try{
-            this.gameEngine.getComponents().forEach(component -> component.render(g));
+            gameEngine.getComponents().forEach(component -> component.render(g));
         }catch (Exception e){
             e.printStackTrace();
         }finally {
-            this.gameEngine.getComponentsLock().unlock();
+            gameEngine.getComponentsLock().unlock();
         }
     }
 
     public void onFocusLost() {
-        this.fpsRunner.setAps(10);
+        fpsRunner.setAps(10);
     }
 
     public void onFocusGained() {
-        this.fpsRunner.setAps(60);
+        fpsRunner.setAps(60);
     }
 }
