746e1f75ab98bdfb7c8a8d86e4cd82b0547e4938
Czekaj Tom <red57101@gmail.com>
2023-09-22 21:54:28 +0200

    :loud_sound: Add logging

--- src/main/java/fr/tyr/game/GameEngine.java
+++ src/main/java/fr/tyr/game/GameEngine.java
@@ -1,8 +1,8 @@
 package fr.tyr.game;
 
-import fr.tyr.components.GameComponent;
-import fr.tyr.components.ImageComponent;
-import fr.tyr.images.Images;
+import fr.tyr.Main;
+import fr.tyr.components.classic.GameComponent;
+import fr.tyr.components.sample.*;
 import fr.tyr.tools.Vector2D;
 
 import java.util.ArrayList;
@@ -14,23 +14,31 @@ public class GameEngine {
     private final boolean devMode;
 
     private final ReentrantLock componentsLock = new ReentrantLock();
-    private final List<GameComponent> components;
+    private final List<GameComponent<?>> components;
 
     public GameEngine(boolean devMode){
+        Main.getLogger().info("Initializing game engine...");
         this.devMode = devMode;
         this.components = new ArrayList<>();
         this.initScene();
+        Main.getLogger().info("Game engine initialized.");
     }
 
     private void initScene(){
+        Main.getLogger().info("Initializing scene...");
         this.componentsLock.lock();
         try{
-            this.components.add(new ImageComponent(new Vector2D(100, 100), Images.SWORDS, 50, 50));
+            this.components.add(new SampleBackgroundComponent());
+            this.components.add(new SampleImageComponent(new Vector2D(100, 100), new Vector2D(50, 50)));
+            this.components.add(new SampleTextComponent(new Vector2D(200, 200)));
+            this.components.add(new SampleAnimatedImageComponent(new Vector2D(300, 300)));
+            this.components.add(new SampleAnimatedTextComponent(new Vector2D(400, 400)));
         }catch (Exception e) {
-            e.printStackTrace();
+            Main.getLogger().severe(e.getMessage());
         }finally {
             this.componentsLock.unlock();
         }
+        Main.getLogger().info("Scene initialized.");
     }
 
     public boolean isDevMode() {
@@ -39,7 +47,7 @@ public class GameEngine {
     public ReentrantLock getComponentsLock() {
         return componentsLock;
     }
-    public List<GameComponent> getComponents() {
+    public List<GameComponent<?>> getComponents() {
         return components;
     }
 }
