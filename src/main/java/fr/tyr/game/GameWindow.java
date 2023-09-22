c85957a984d8848fadcc552b167b0a4e733df05c
Czekaj Tom <red57101@gmail.com>
2023-09-22 21:53:57 +0200

    :bug: Fix window name

--- src/main/java/fr/tyr/game/GameWindow.java
+++ src/main/java/fr/tyr/game/GameWindow.java
@@ -1,8 +1,6 @@
 package fr.tyr.game;
 
 import javax.swing.*;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowFocusListener;
 
 public class GameWindow extends JFrame {
 
@@ -11,19 +9,6 @@ public class GameWindow extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1280, 720);
         this.setLocationRelativeTo(null);
-        this.setResizable(false);
-
-        this.addWindowFocusListener( new WindowFocusListener() {
-            @Override
-            public void windowLostFocus(WindowEvent e) {
-                graphicEngine.onFocusLost();
-            }
-            @Override
-            public void windowGainedFocus(WindowEvent e) {
-                graphicEngine.onFocusGained();
-            }
-        });
-
         this.setVisible(true);
     }
 
