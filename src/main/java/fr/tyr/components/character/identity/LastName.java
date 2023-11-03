bb7848adb3929a8c7a9622d770e42e5cb8eac7a9
Dim_Toups <dimitri.toupense@etu.unistra.fr>
2023-11-03 18:03:06 +0100

    :bento: Add a list of firstnames and lastnames for characters

--- src/main/java/fr/tyr/components/character/identity/LastName.java
+++ src/main/java/fr/tyr/components/character/identity/LastName.java
@@ -1,7 +1,33 @@
 package fr.tyr.components.character.identity;
 
 public enum LastName {
-    ;
+    MARTIN("Marthin"),
+    PETIT("Petit"),
+    GIRAUD("Giraud"),
+    MEYER("Meyer"),
+    MULLER("Muller"),
+    SCHMITT("Schmitt"),
+    SCHNEIDER("Schneider"),
+    JUNG("Jung"),
+    ROTH("Roth"),
+    PETER("Peter"),
+    HEINRICH("Heinrich"),
+    KUHN("Kuhn"),
+    OTT("Ott"),
+    HARTMANN("HARTMANN"),
+    LANG("Lang"),
+    FISCHER("Fischer"),
+    AZIZ("Aziz"),
+    AL_BAKIR("Al Bakir"),
+    FAROUK("Farouk"),
+    SMITH("Smith"),
+    BROWN("Brown"),
+    LOPEZ("Lopez"),
+    MARTINEZ("Martinez"),
+    LIN("Lin"),
+    ZHANG("Zhang"),
+    WU("Wu"),
+    CHEN("Chen");
     private final String lastName;
 
     /**
