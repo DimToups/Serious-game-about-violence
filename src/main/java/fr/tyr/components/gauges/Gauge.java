67be72a72614959249b9d9dfb33c212fddd3ef70
Czekaj Tom <red57101@gmail.com>
2023-12-20 22:38:01 +0100

    :fire: Remove gauge testing behavior

--- src/main/java/fr/tyr/components/gauges/Gauge.java
+++ src/main/java/fr/tyr/components/gauges/Gauge.java
@@ -31,14 +31,12 @@ public class Gauge extends ComposedComponent {
     public void setCurrentProgress(int newProgress) {
         currentProgress = newProgress;
         int gaugeSize = 100 - newProgress;
-        Main.getLogger().warning("Gauge size: " + gaugeSize);
+        Main.getLogger().info("%s set to %d%%".formatted(getClass().getSimpleName(), newProgress));
         if(newProgress == 0){
-            Main.getLogger().warning("Gauge is empty");
             progressBar.setVisible(false);
             return;
         }else
             progressBar.setVisible(true);
-
         if(isVertical){
             int y = (int) (progressBar.getOriginalSize().y * (gaugeSize / 100D));
             progressBar.crop(0, y, (int) progressBar.getOriginalSize().x, (int) progressBar.getOriginalSize().y - y);
@@ -47,12 +45,5 @@ public class Gauge extends ComposedComponent {
             int x = (int) (progressBar.getOriginalSize().x * (gaugeSize / 100D));
             progressBar.crop(0, 0, (int) progressBar.getOriginalSize().x - x, (int) progressBar.getOriginalSize().y);
         }
-    }
-
-    @Override
-    public void onClick(MouseButtons button) {
-        int random = new Random().nextInt(0, 101);
-        Main.getLogger().warning("Random: " + random);
-        setCurrentProgress(random);
     }
 }
